2f66ad673ef9b06ceb16eee293c6888270f6b601
user <user@user-PC>
2015-05-11 11:12:48 +0900

    launching..

---
+++ jse/src/cmm01/var/Var.java
@@ -0,0 +1,27 @@
+package cmm01.var;
+
+class Var{
+
+ public static void main(String []arg) {
+  /*
+   * 논리타입 : boolean 
+   * 정수형 : int
+   * 실수형 : float , double
+   * */
+  int x;
+  
+  x= 1;
+  
+  int y;
+  
+  y= x+1;
+  
+  
+  
+  System.out.println();
+  /*
+   * "" 문자 출력 , C언어와다르게 바로 변수 적용가능 */
+  System.out.println("x+1의 값"+y);
+ }
+ 
+}
