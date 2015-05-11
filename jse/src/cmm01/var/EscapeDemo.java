6d3c407ef47acd9ca5f567c94b39cdc82cf0d5cb
user <user@user-PC>
2015-05-11 11:55:36 +0900

    escape study

---
+++ jse/src/cmm01/var/EscapeDemo.java
@@ -0,0 +1,28 @@
+package cmm01.var;
+
+/*
+Date : 2015.05.11
+Author : 양문석
+Description : 이스케이프 문법에 대한 학습 자료 
+* */
+
+
+public class EscapeDemo {
+	public static void main(String[] args) {
+		
+		int kor = 90;
+		int eng = 85;
+		int sum = 0;
+		
+		
+		sum = kor + eng;
+		
+		
+		System.out.println("\t성적처리");
+		System.out.println("=================================");
+		System.out.println("국어 + 영어 + 총점 ");
+		System.out.println(kor +" + "+ eng +" = "+ sum);
+		
+	}
+
+}
